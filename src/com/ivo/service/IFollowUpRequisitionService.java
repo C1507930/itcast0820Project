275a96adeff8c8e753d6f2e3135fc1afd7cb2078
C1507930 <1990145701@qq.com>
2015-09-13 19:10:17 +0800

    Create IFollowUpRequisitionService.java

---
+++ src/com/ivo/service/IFollowUpRequisitionService.java
@@ -0,0 +1,7 @@
+package com.ivo.service;
+
+import com.ivo.domain.FollowUpRequisition;
+
+public interface IFollowUpRequisitionService {
+ void saveFollowUpRequisition(FollowUpRequisition followUpRequisition);
+}
