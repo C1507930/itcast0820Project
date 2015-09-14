693f1a8ac1a755da67f1f5ceb263c781161b2f8b
C1507930 <1990145701@qq.com>
2015-09-14 22:00:46 +0800

    Update BaseInterceptor.java

--- src/com/ivo/web/interceptor/BaseInterceptor.java
+++ src/com/ivo/web/interceptor/BaseInterceptor.java
@@ -3,6 +3,7 @@ package com.ivo.web.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import com.ivo.domain.Employee;
@@ -15,16 +16,34 @@ public class BaseInterceptor extends HandlerInterceptorAdapter{
 	public boolean preHandle(HttpServletRequest request,
 			HttpServletResponse response, Object handler) throws Exception {
 		// TODO Auto-generated method stub
+		
+		
+		System.out.println("拦截");
 		if(handler instanceof BaseController){
 			//拦截请求，把session中的用户name获取出来
 			//Employee employee = (Employee) request.getSession().getAttribute("name");
 		}
 		
 		 //注入Request/Response对象至SpringMVC执行上下文对象中
-	     ControllerContext.getContext().setRequest(request);
-	      ControllerContext.getContext().setResponse(response);
+		if(ControllerContext.getContext().getContextMap().isEmpty()){
+			ControllerContext.getContext().setRequest(request);
+			ControllerContext.getContext().setResponse(response);
+			//System.out.println(ControllerContext.getContext().getRequest().toString());
+			
+		}
 		
 		return super.preHandle(request, response, handler);
 	}
 
+	
+	public void postHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println(ControllerContext.getContext().getRequest().getSession().getAttribute("name"));
+		super.postHandle(request, response, handler, modelAndView);
+	}
+
+
+	
 }
